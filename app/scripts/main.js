1f38aba1d666e3975a41af0d6884676879a701ad
Ben Aneesh <benaneesh@gmail.com>
2013-11-16 18:33:50 -0500

    fixed to add text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,21 @@
 //JSON object with the data
 var treeData1 = [
- { 
+{
    id: "42afba9dae229e85606b75b8e4e8d3443edaf237",
    parent: undefined
- },
- {
+},
+{
    id: "3a1d98d866bfc0b372bd7a15c8f07c67319697c6",
-   parent: "42afba9dae229e85606b75b8e4e8d3443edaf237" 
- },
- { 
+   parent: "42afba9dae229e85606b75b8e4e8d3443edaf237"
+},
+{
    id: "1a410efbd13591db07496601ebc7a059dd55cfe9",
    parent: "42afba9dae229e85606b75b8e4e8d3443edaf237"
- },
- { 
+},
+{
    id: "123456d866bfc0b372bd7a15c8f07c67319697c6",
    parent: "3a1d98d866bfc0b372bd7a15c8f07c67319697c6"
- }
+}
 ];
 
 //var treeData = convertGitObject(treeData1);
@@ -31,66 +31,71 @@ var vis = d3.select("#viz").append("svg:svg")
                            .append("svg:g")
                            .attr("transform", "translate(40, 0)"); // shift everything to the right
 
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(60)
+    .size([width, height]);
+
+//var svg = d3.select("body").append("svg")
+//    .attr("width", width)
+//    .attr("height", height);
+
+
+
 // Create a tree "canvas"
 var tree = d3.layout.tree()
   .size([h*0.8,w*0.8]);
 
-d3.json("scripts/commits.json", function(json) {
+d3.json("scripts/commits.json", function(graph) {
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
-  var force = d3.layout.force()
-                       .charge(-120)
-                       .distance(60)  
-                       .gravity(.1)
-                       .nodes(json.nodes)
-                       .links(json.links)
-                       .size([w, h])
-                       .start();
+    var link = vis.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  var link = vis.selectAll('line.link')
-    .data(json.links)
-    .enter().append('svg:line')
-    .attr("class", "link")       
-    .style('stroke-width', 3)
-    .attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
+    var gnodes = vis.selectAll('g.gnode')
+        .data(graph.nodes)
+        .enter()
+        .append('g')
+        .classed('gnode', true);
 
-  var node = vis.selectAll("circle.node")
-    .data(json.nodes)
-    .enter().append("svg:circle")
-    .attr("class", "node")
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("r", 5)
-    .on('mouseover', function(d){
-      console.log(d);
-          var nodeSelection = d3.select(this).style({opacity:'0.8'});
-              nodeSelection.select("text").style({opacity:'1.0'});
-    })
-    .style("fill", function(d) { return fill(d.group); })
-    .call(force.drag);
+    var node = gnodes.append("circle")
+        .attr("class", "node")
+        .attr("r", 5)
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
 
-  node.append("text")
-    .attr("dx", 12)
-    .attr("dy", ".35em")
-    .text(function(d) { return d.name; });
+    var labels = gnodes.append("text")
+        .text(function(d) { return d.name; });
 
-  force.on("tick", function(e) { 
+    console.log(labels);
 
-    var k = .5 * e.alpha; 
-    node.each(function(d) { 
-      d.x += ((4 + d.group) * 100 - d.x) * k; 
+    force.on("tick", function(e) {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        gnodes.attr("transform", function(d) {
+            return 'translate(' + [d.x, d.y] + ')';
+        });
+
+        var k = .5 * e.alpha;
+        node.each(function(d) {
+            d.x += ((4 + d.group) * 100 - d.x) * k;
+        });
+
     });
-
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-  node.attr("cx", function(d) { return d.x ; })
-      .attr("cy", function(d) { return d.y; });
-  });
 });
 
 var treeData = {
@@ -242,3 +247,9 @@ vis.selectAll(".lonely")
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; })
     .call(circleDragger)
+
+
+
+
+//var drawGraph = function(graph) {
+
