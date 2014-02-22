05455bd2789f5d543ed16138f6033cc4ed5112c5
jcytong <mailbox@johnnytong.com>
2014-02-22 14:53:26 -0500

    Adds skeleton for grid representation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -83,9 +83,39 @@ function draw_branch(beginning, end) {
   y2 = y1 + 150 * scale;
 }
 
-var head = master.head;
-render(master.head, 50);
+function renderGraph(points, config) {
+  _.each(points, function(p){
+    console.log('rendering ' + p.id + ' on column: ' + p.col + ', row: ' + p.row);
+  });
+}
 
-cur_x = 750;
-head = feature1.head;
-render(feature1.head, 200);
+function walkBranch(head, max_depth) {
+  var result = [];
+  return result;
+}
+
+function walkTree(branches, max_depth) {
+  var branchIdx = 0;
+  var result = [];
+  _.each(branches, function(branch) {
+    result.concat(result, walkBranch(branch.head, max_depth);
+    branchIdx++;
+  });
+}
+
+function Point(id, x, y) {
+  this.id = id;
+  this.x = null;
+  this.y = null;
+}
+
+(function(){
+  var MAX_COLUMNS = 10;
+  //pass 1 - assign column
+  var fooResults = walkTree(branches, MAX_COLUMNS);
+  //pass 2 - render commits
+  renderGraph(fooResults, {
+    columns: MAX_COLUMNS,
+    rows: 2
+  });
+})();
