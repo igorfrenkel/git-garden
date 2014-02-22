0cb416bbf0446fdcbd26cf6ecd9d2c7e17820f67
jcytong <mailbox@johnnytong.com>
2014-02-22 17:22:39 -0500

    Integrates frame-interface

--- app/scripts/frame-interface.js
+++ app/scripts/frame-interface.js
@@ -1,3 +1,21 @@
+function CanvasCommit(id, cp) {
+  this.id = id;
+  this.point = cp;
+}
+
+function CanvasPoint(col, row) {
+  this.row = row;
+  this.col = col;
+}
+
+function CanvasBranch(p0, p1) {
+  this.p0 = p0;
+  this.p1 = p1;
+}
+
+function OffCanvasBranch() {
+}
+
 function Frame(commits, branches, connectors) {
   this.commits = commits;
   this.branches = branches;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,39 @@
-  function renderGraph(points, config) {
-  _.each(points, function(p){
-    console.log('rendering ' + p.id + ' on column: ' + p.col + ', row: ' + p.row);
+function renderGraph(frame, config) {
+  _.each(frame.commits, function(commit) {
+    //render commit
+    console.log('rendering ' + commit.id + ' on column: ' + commit.point.col + ', row: ' + commit.point.row);
+  });
+  _.each(frame.branches, function(branch) {
+    //render branch
+    console.log('rendering branch from: ' + branch.p0 + ' to ' + branch.p1);
+  });
+  _.each(frame.connectors, function(connector) {
+    //render connector
+    console.log('rendering connector from: ' + connector.p0 + ' to ' + connector.p1);
   });
 }
 
 function walkBranch(cur_commit, branchIdx, depth) {
-  var p = new CanvasCommit(cur_commit.id, depth-1, branchIdx);
-  if(depth == 0 || cur_commit.parent == null) {
-    return [p];
-  }
-  else {
-    return [p].concat(walkBranch(cur_commit.parent, branchIdx, depth-1));
-  }
+  var p = new CanvasCommit(cur_commit.id, new CanvasPoint(depth-1, branchIdx));
+  if(depth == 0 || cur_commit.parent == null) return [p];
+  else return [p].concat(walkBranch(cur_commit.parent, branchIdx, depth-1));
 }
 
 function walkTree(branches, max_depth) {
   var branchIdx = 0;
-  var result = [];
+  var commits = c_branches = connectors = [];
   _.each(branches, function(branch) {
-    result = result.concat(walkBranch(branch.head, branchIdx, max_depth));
+    commits = commits.concat(walkBranch(branch.head, branchIdx, max_depth));
     branchIdx++;
   });
-  return result;
-}
-
-function CanvasCommit(id, col, row) {
-  this.id = id;
-  this.col = col;
-  this.row = row;
+  return new Frame(commits, c_branches, connectors);
 }
 
 var MAX_COLUMNS = 10;
 //pass 1 - assign column
-var fooResults = walkTree(branches, MAX_COLUMNS);
+var frame = walkTree(branches, MAX_COLUMNS);
 //pass 2 - render commits
-renderGraph(fooResults, {
+renderGraph(frame, {
   columns: MAX_COLUMNS,
   rows: 2
 });
